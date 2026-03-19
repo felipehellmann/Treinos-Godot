a320e1c2508b68c72f862d66c67152dd2069f54f
Felipe Hellmann <hellmannmaionese@gmail.com>
2026-03-18 22:23:01 -0300

    Adicionei um codigo de pulo duplo ao jogo

--- codigo-de-movimento/character_body_2d.gd
+++ codigo-de-movimento/character_body_2d.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0
+var jumps = 2
 
 
 func _physics_process(delta: float) -> void:
@@ -13,6 +14,14 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+
+	# double jump \ pulo duplo moleque hehehe
+	if Input.is_action_just_pressed("ui_accept") and not is_on_floor() and jumps < 2:
+		velocity.y = JUMP_VELOCITY
+		jumps = 2
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
